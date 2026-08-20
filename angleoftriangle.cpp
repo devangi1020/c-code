775466c57220c2279a92c63ef213d496bfba0162
devangi1020 <devangithakur2010@gmail.com>
2026-08-20 17:38:26 +0530

    angle of triangle

---
+++ angleoftriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a1, a2, a3;
+	cout<<"enter the value of angels:";
+	cin>>a1>>a2>>a3;
+	if((a1+a2+a3)==180)
+	cout<<"this is a triangle";
+	else
+	cout<<"this is not a triangle";
+	return 0;
+}
