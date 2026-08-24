65bc43293b0ae01f6ef1f90a291bead130eda7da
Devangi Thakur <devangithakur2010@gmail.com>
2026-08-24 22:48:47 +0530

    code for diameter, circumference and area of circle

---
+++ DIA,CIR,ARE-OFCIRCLE.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float d, c, a, r;
+	cout<<"enter r";
+	cin>>r;
+	d= 2*r;
+	c= 2*2.14*r;
+	a= 2.14*r*r;
+	cout<<"the resut is "<<d<<endl;
+	cout<<"the result is "<<c<<endl;
+	cout<<"the result is "<<a<<endl;
+	return 0;
+}
