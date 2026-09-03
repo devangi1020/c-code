8ce0d80fc93533c6a91e0f7f8a4717175d99cf93
Devangi Thakur <devangithakur2010@gmail.com>
2026-09-03 23:10:36 +0530

    tell whether the number is positive or negative

---
+++ positiveandnegative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the num ";
+	cin>>a;
+	if(a<0)
+	{
+		cout<<"the number is negative ";
+	}
+	else 
+	cout<<"the number is positive ";
+	return 0;
+}
